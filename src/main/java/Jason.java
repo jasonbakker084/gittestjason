33e9da70555397ad89186a10c86cd7a8d24b763b
Jason Bakker <jasonbakker084@gmail.com>
2019-09-10 10:14:31 +0200

    6th commit

--- src/main/java/Jason.java
+++ src/main/java/Jason.java
@@ -3,14 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.novi.oefeninggittest;
 
 /**
  *
  * @author jasonbakker
  */
-public class TestClasse {
- int x = 5;
- String y = "Ik ben een String";
- boolean Jason = true;
+public class Jason {
+    int f =  6;
 }
